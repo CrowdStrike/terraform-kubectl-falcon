427e5e70b45e493cdeee8d84e0188bfb0651f850
ffalor <ffalorjr@outlook.com>
2023-04-20 17:21:49 -0500

    add an existing eks cluster example

---
+++ examples/aws-existing-eks-cluster/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = var.region
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count      = 10
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+module "crowdstrike_falcon" {
+  source = "../.."
+
+  cid              = var.cid
+  client_id        = var.client_id
+  client_secret    = var.client_secret
+  cloud            = var.cloud
+  cluster_name     = data.aws_eks_cluster.this.arn
+  docker_api_token = var.docker_api_token
+}
