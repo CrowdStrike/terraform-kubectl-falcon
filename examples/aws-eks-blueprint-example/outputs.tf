c27e6c1d64d85e1e5eaa27100a6f5e143f916d96
Ryan Payne <ryanj.payne@outlook.com>
2023-09-19 11:27:42 -0500

    fix output reference

--- examples/aws-eks-blueprint-example/outputs.tf
+++ examples/aws-eks-blueprint-example/outputs.tf
@@ -1,4 +1,4 @@
 output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = "aws eks update-kubeconfig --region ${var.region} --name ${eks.cluster_name}"
+  value       = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}"
 }
