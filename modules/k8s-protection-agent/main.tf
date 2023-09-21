62b49256a6932884bad41bb25b06b8af0e2a350c
Ryan Payne <ryanj.payne@outlook.com>
2023-09-21 09:14:12 -0500

    lower cid with native functions

--- modules/k8s-protection-agent/main.tf
+++ modules/k8s-protection-agent/main.tf
@@ -1,17 +1,6 @@
-resource "null_resource" "lower_cid" {
-  provisioner "local-exec" {
-    command = <<-EOF
-      cid=${var.cid}
-      trim_cid=$(echo $cid | rev | cut -c 4- | rev)
-      my_cid=$(echo $trim_cid | tr '[:upper:]' '[:lower:]')
-      printf $my_cid > cid.txt
-    EOF
-  }
-}
-
-data "local_file" "lower_cid" {
-    filename = "cid.txt"
-  depends_on = [null_resource.lower_cid]
+locals {
+  lower_cid = "${lower(var.cid)}"
+  cid_split = "${split("-", local.lower_cid)}"
 }
 
 resource "helm_release" "kpagent" {
@@ -44,7 +33,7 @@ resource "helm_release" "kpagent" {
 
   set {
     name  = "crowdstrikeConfig.cid"
-    value = data.local_file.lower_cid.content
+    value = local.cid_split[0]
   }
 
   set {
