e5f5683003c1811b7f7d58d51df4ca537fb4f689
Ryan Payne <ryan.payne@crowdstrike.com>
2025-04-04 09:54:02 -0500

    operator version

--- variables.tf
+++ variables.tf
@@ -57,7 +57,7 @@ variable "environment" {
 variable "operator_version" {
   description = "Falcon Operator version to deploy. Can be a branch, tag, or commit hash of the falcon-operator repo."
   type        = string
-  default     = "v0.9.1"
+  default     = "v1.4.0"
 }
 
 variable "node_sensor_mode" {
